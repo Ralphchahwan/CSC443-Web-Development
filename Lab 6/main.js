2c0bf85d35edeff15736418f3f4738f74165884d
Ralph Chahwan <ralph@Ralphs-MacBook-Pro.local>
2018-10-20 22:18:01 +0300

    sprint 2 lab 6

--- Lab 6/main.js
+++ Lab 6/main.js
@@ -3,6 +3,8 @@ var down = document.querySelector(".down");
 
 var sources = ["1.jpeg","2.jpeg","3.jpeg","4.jpeg","5.jpeg","6.jpeg","7.jpeg","8.jpeg","9.jpeg","10.jpeg"]; //original array
 
+var picsrc = [];
+
 var x = parseInt(prompt("Enter the number of cards you want"));
 
 function shuffle(a) { //shuffle function
@@ -14,27 +16,33 @@ function shuffle(a) { //shuffle function
 }
 
 var newsource = []; //create new array
+var newsource1 = [];
 
 for (var i = 0; i < x; i++) {
   newsource[i] = sources[i];
 }
 
+for (var i = 0; i < x; i++) {
+  newsource1[i] = sources[i];
+}
+
 
 var upper = shuffle(newsource); //array of images for upper
-var lower = shuffle(newsource); //array of images for lower
+var lower = shuffle(newsource1); //array of images for lower
 
 var cardsa = []; //array of cards for up
 var cardsb = []; //array of cards for down
 var temp = null;
 
+
 for (let i = 0; i < x; i++) {
-  temp = new Card(upper[i],i);
-  cardsa.push(temp);  //creates an array of cards
-  up.append(temp.imgHTML);
+  temp = new Card(upper[i]);
+  cardsa.push(temp);  //adds to the array of cardsa
+  up.appendChild(temp.imgHTML);
 }
 
 for (let i = 0; i < x; i++) {
-  temp = new Card(lower[i],i);
-  cardsb.push(temp);  //creates an array of cards
-  down.append(temp.imgHTML);
+  temp = new Card(lower[i]);
+  cardsb.push(temp);  //adds to the array of cardsb
+  down.appendChild(temp.imgHTML);
 }
--- Lab 6/memorygame.js
+++ Lab 6/memorygame.js
@@ -1,17 +1,63 @@
 class Card {
 
-  constructor(src,id) {
+  constructor(src) {
 
     this.initialSrc = "illusion.jpeg";
     this.src = src;
-    this.id = id;
+
+
     this.imgHTML = document.createElement("img");
     this.imgHTML.src =this.initialSrc;
 
+   this.show = this.show.bind(this);
+   this.imgHTML.addEventListener("click",this.show);
+
+   this.reset = this.reset.bind(this);
+   this.remove = this.remove.bind(this);
+
   }
 
+  show() {
+  this.imgHTML.src = this.src;
+  picsrc.push(this);
+
+
+  if(picsrc.length === 2) {
+    console.log(picsrc[0].src);
+    console.log(picsrc[1].src);
 
 
 
+    if(picsrc[0].src === picsrc[1].src){
+      setTimeout(this.remove,500);
+    }
+    else {
+      setTimeout(this.reset,500);
+          }
+        }
+      }
+
+       remove(){
+        picsrc[0].imgHTML.style.display="none";
+        picsrc[1].imgHTML.style.display="none";
+        picsrc = [];
+      }
+
+      reset(){
+        picsrc[0].imgHTML.src = "illusion.jpeg";
+        picsrc[1].imgHTML.src = "illusion.jpeg";
+        picsrc = [];
+      }
+
+// reset(event) {
+
+
+// }
+
+// hide(event){
+//   picsrc[0].imgHTML.hide();
+//   picsrc[1].imgHTML.hide();
+//   picsrc = [];
+// }
 
 }
